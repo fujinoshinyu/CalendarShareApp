f5c29b142732afc037f20fc22f0fa546953756f4
fujinoshinyu <sincan.1911@gmail.com>
2023-12-18 13:48:27 +0900

    commit4

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,20 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     
-public function getPaginateByLimit(int $limit_count = 10)
+public function getPaginateByLimit(int $limit_count = 5)
 {
-    return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+return $this::with('category')->orderBy('updated_at', 'DESC')->paginate($limit_count);
     // updated_atで降順に並べたあと、limitで件数制限をかける
 }
     protected $fillable = [
     'title',
     'body', 
+    'category_id'
 ];
 
+// Categoryに対するリレーション
+//「1対多」の関係なので単数系に 
+public function category() 
+{
+return $this->belongsTo(Category::class); 
+    
+}
 }
 
